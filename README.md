774d09e88e5e0feefd544f9be739b7b2ab7f8aab
Daijiro Wachi <daijiro.wachi@gmail.com>
2015-01-17 01:52:01 +0900

    Update README and test code

--- README.md
+++ README.md
@@ -2,3 +2,37 @@ optimin
 =======
 
 A Library for parsing option and building cli
+
+## Install
+with npm
+```
+$ npm install --save optmin
+```
+
+## Usage
+index.js
+```js
+var argv = require('optmin')(process.argv.slice(2), {
+  aaa: {
+    alias: 'a',
+    boolean: true
+  },
+  bbb: {
+    alias: 'b',
+    default: 123
+  },
+  ccc: {
+    alias: 'c'
+  },
+  ddd: {
+    alias: 'd'
+  }
+});
+console.log(argv);
+```
+
+then
+```
+$ node index.js
+{ bbb: 123, aaa: true, ccc: 'hello', ddd: 'world' }
+```
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-var command = 'node test.js -a -c 123 --ddd 123'.split(' ');
+var command = 'node test.js -a -c hello --ddd world'.split(' ');
 var argv = require('./')(command.slice(2), {
   aaa: {
     alias: 'a',
@@ -16,7 +16,9 @@ var argv = require('./')(command.slice(2), {
   }
 });
 
+console.log(argv);
+
 if (argv.aaa !== true) throw new Error();
 if (argv.bbb !== 123) throw new Error();
-if (argv.ccc !== '123') throw new Error();
-if (argv.ddd !== '123') throw new Error();
+if (argv.ccc !== 'hello') throw new Error();
+if (argv.ddd !== 'world') throw new Error();
